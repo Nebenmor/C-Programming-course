0478f48024a99618feb5ab3ba6e2987f2fbec3a2
Nebenmor <nebenmor.anthony@gmail.com>
2023-10-03 15:50:04 +0100

    using the bitwise operators

---
+++ case_8.5.c
@@ -0,0 +1,22 @@
+/* using bitwise operators */
+
+#include <stdio.h>
+
+main()
+{
+	int x, y, z;
+
+	x = 4321;
+	y = 5678;
+
+	printf("Given x = %u, i.e., 0X%04X\n", x, x);
+	printf("Given y = %u, i.e., 0x%04x\n", y, y);
+	z = x & y;
+	printf("x & y returns: %6u, i.e., 0X%04X\n", z, z);
+	z = x|y;
+	printf("x | y returns: %6u, i.e., 0X%04X\n", z, z);
+	z = x^y;
+        printf("x ^ y returns: %6u, i.e., 0X%04X\n", z, z);
+        printf("~x returns: %6u, i.e., 0X%04X\n", ~x, ~x);
+	return 0;
+}
